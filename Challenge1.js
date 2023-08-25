d3d6acccf8a31781ab33f88e0e90fdad5de57423
DanielGuezo <126103587+DanielGuezo@users.noreply.github.com>
2023-08-25 18:08:05 +0200

    Add files via upload

---
+++ Challenge1.js
@@ -0,0 +1,15 @@
+const firstName = 'John';
+const age = 35;
+const hobby1 = 'Coding';
+
+function hobby () {
+console.log(`Hello, ${firstName} (${age}). I love ${hobby1}!`)
+}
+
+const logTwice = () => {
+  console.log(hobby);
+
+}
+
+hobby()
+hobby()
